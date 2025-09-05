8d3bc5d6691a31e7ae29a925cb41d58ec8822c35
ksalik404-cyber <ksalik404@gmail,com>
2025-09-05 21:49:13 +0530

    saving local changes before merge

---
+++ Gitandgithubcommands.txt.py
@@ -0,0 +1,7 @@
+
+git config --global user.name "ksalik404-
+git config --global user.email "ksalik404@gmail,com"
+git config --global color.ui auto
+git init
+git remote add origin https://github.com/ksalik404-cyber/gitDemo.git
+git pull origin main
