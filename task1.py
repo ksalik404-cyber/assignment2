2d43aa8f2c5f40bf955bd34b0ebb2c5df6bbd1d9
ksalik404-cyber <ksalik404@gmail.com>
2025-09-07 00:44:39 +0530

    Add task1.p and task2.py only

---
+++ task1.py
@@ -0,0 +1,8 @@
+#TASK 1 CHECKING IF A NUMBER IS EVEN OR ODD
+n=int(input("number:"))
+if n % 2==0:
+   print("number is even")
+else:
+   print("number is odd")
+
+   
---
+++ task2.py
@@ -0,0 +1,6 @@
+#SUM OF INTEGERS FROM 1-50
+total_sum = 0
+for i in range(1, 51):
+    total_sum += i
+
+print("The sum of numbers from 1 to 50 is:", total_sum)
