ce483fd92c037e77f4330c51ff69b8b9f479752c
ksalik404-cyber <ksalik404@gmail.com>
2025-09-07 20:09:55 +0530

    saving local changes before merge

---
+++ assigment1/2/__init__.py
@@ -0,0 +1,6 @@
+# task two : perspnalized greeting
+firstname=input(" please enter your firstname:")
+lastname=input(" please enter your lastname:")
+greeting="Hello,"+ firstname+" "+ lastname + "!welcome to Python course."
+print(greeting)
+
---
+++ assigment1/__init__.py
@@ -0,0 +1,13 @@
+#task 1 basic mathematical operations
+a=input('a:')
+b=input('b:' )
+a=float(a)
+b=float(b)
+x1=a+b
+x2=a-b
+x3=a*b
+x4=a/b
+print('addition:',x1)
+print('subtraction:',x2)
+print('multiplication:',x3)
+print('division:',x4)
